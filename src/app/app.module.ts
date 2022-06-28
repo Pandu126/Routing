373d9f0cd9d81462883a850b86b331cec1070222
Pandu126 <fun2word@gmail.com>
2022-06-28 16:20:58 +0530

    component
    Componnets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    LoginComponent,
+    HomeComponent,
+    CategoriesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
