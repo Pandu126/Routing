6998b39fc0524900dc27d45d2363eb6410f9c262
Pandu126 <fun2word@gmail.com>
2022-07-01 16:13:56 +0530

    Update randomUser.service.ts

--- src/app/services/randomUser.service.ts
+++ src/app/services/randomUser.service.ts
@@ -1,17 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs';
 
 @Injectable()
 export class RandomUser {
-//   post: any;
-interval:number;
   constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    // this.getPost();
-  }
   getPost() {
-    return this.http.get('https://randomuser.me/api/')
+    return this.http.get('https://randomuser.me/api/');
   }
 }
