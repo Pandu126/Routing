570b5670f0b89af4eeb4d2b7de56ded98bb97b6e
Pandu126 <fun2word@gmail.com>
2022-07-01 16:06:22 +0530

    Update random-user.component.ts

--- src/app/random-user/random-user.component.ts
+++ src/app/random-user/random-user.component.ts
@@ -1,6 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs';
 import { RandomUser } from '../services/randomUser.service';
 
 @Component({
@@ -10,7 +8,7 @@ import { RandomUser } from '../services/randomUser.service';
 })
 export class RandomUserComponent implements OnInit {
   profileDetails: any;
-  constructor(private randomUser: RandomUser, private http: HttpClient) {}
+  constructor(private randomUser: RandomUser) {}
 
   ngOnInit(): void {
     this.getProfile();
@@ -18,13 +16,6 @@ export class RandomUserComponent implements OnInit {
   nextUser() {
     this.getProfile();
   }
-  // getProfile() {
-  //   return this.http.get('https://randomuser.me/api/').subscribe((res) => {
-  //     this.profile = res;
-  //     this.profileDetails = this.profile['results'][0];
-  //     console.log(this.profile);
-  //     console.log(this.profileDetails);
-  //   });
   getProfile() {
     return this.randomUser.getPost().subscribe((Response: any) => {
       return this.profileDetails=Response['results'][0]
